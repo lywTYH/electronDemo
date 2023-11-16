ea06435e8ce371733f409e602ad017e72d3548ea
lyw <763613923@qq.com>
2023-11-16 13:27:44 +0800

    init

---
+++ scripts/build.js
@@ -0,0 +1,11 @@
+import gulp from "gulp";
+import '../gulpfile.js';
+function build() {
+  const taskInstance = gulp.task('build');
+  if (taskInstance === undefined) {
+    console.error('no task named compile registered');
+    return;
+  }
+  taskInstance.apply(gulp);
+}
+build()
