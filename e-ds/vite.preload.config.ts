27445567d697c143a78af39d1ad2daf899947e37
lywTYH <763613923@qq.com>
2026-06-08 15:23:22 +0800

    fix: add preload build config

--- e-ds/vite.preload.config.ts
+++ e-ds/vite.preload.config.ts
@@ -1,4 +1,13 @@
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        // Avoid naming collision with the main process entry (both are index.ts).
+        entryFileNames: 'preload.js',
+      },
+    },
+  },
+});
